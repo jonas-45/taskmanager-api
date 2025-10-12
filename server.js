ef55d4151bdf3efc179c4bf4e3fb24e52478e3db
jonas-45 <wullahjonas@gmail.com>
2025-10-12 00:41:05 +0000

    Add signup and login views

--- server.js
+++ server.js
@@ -7,18 +7,22 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
     .catch(err => console.log(`DB connection error: ${err}`))
 
 const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.send('You are welcome');
+    res.redirect('./login');
 })
 
 app.get('/signup', (req, res) => {
-
+    res.render('signup', {title: 'Signup'});
 })
 
 app.get('/login', (req, res) => {
-    
+    res.render('login', {title: 'Login'});
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

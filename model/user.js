5f194917b41ccd9f7575446bec218c1f6becc031
jonas-45 <wullahjonas@gmail.com>
2025-10-12 00:06:19 +0000

    Add user db schema and model

---
+++ model/user.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const userModel = mongoose.model('user', userSchema);
+
+export default userModel;
